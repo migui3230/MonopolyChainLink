3e32bd42a86d088929cb4a520d4e895a45be4ef2
Miguel Sarenas <migui0323@gmail.com>
2022-05-07 22:33:59 -0400

    Add openzeppelin contracts and create contract to mint NFT

--- contracts/Minting.sol
+++ contracts/Minting.sol
@@ -2,21 +2,27 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Greeter {
-    string private greeting;
+contract Minting is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
+    constructor() ERC721("House", "HSE") {}
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }    
+
+
 }
